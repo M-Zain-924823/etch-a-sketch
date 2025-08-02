89df548752680732b70606fd4f443f588e3d67b7
M-Zain-924823 <mz924823@gmail.com>
2025-08-02 17:52:01 +0500

    add custom input grid

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 let container = document.querySelector(".container");
-
-for(let i=0; i<16; i++){
-    for(let j=0; j<16; j++){
-        let div = document.createElement("div");
-        container.appendChild(div);
-    }
-}
+let gridLength = 16;
 
 let divNodes = document.querySelectorAll(".container > *");
-divNodes.forEach(node => {
-    node.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = "black";
-        e.target.style.border = "1px solid white";
-    })
+drawgrid();
+
+function drawgrid(){
+    container.innerHTML = "";
+    for(let i=0; i<gridLength; i++){
+        for(let j=0; j<gridLength; j++){
+            let div = document.createElement("div");
+            container.appendChild(div);
+        }
+    }
+    divNodes.innerHTML = "";
+    divNodes = document.querySelectorAll(".container > *");
+    hoverEffect();
+}
+
+function hoverEffect(){
+    divNodes.forEach(node => {
+        node.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "black";
+            e.target.style.border = "1px solid white";
+        })
+    });
+}
+
+let input = document.querySelector("input");
+let btn = document.querySelector("button");
+input.addEventListener('keydown', (event) => {
+    if (event.key == "Enter") {
+        gridSize(event);
+    }
+});
+btn.addEventListener("click", (event) => {
+    gridSize(event);
 });
 
+function gridSize(e) {
+    if(input.value > 0 && input.value <= 100){
+        gridLength = input.value;
+        drawgrid();
+    }
+}
